a9aa3da8ba813054e341adc0ae3bddbe92535984
darklord <github@dark-lord.de>
2022-03-16 18:51:36 +0100

    3.0

--- dynamicdns.sh
+++ dynamicdns.sh
@@ -1,2 +1,31 @@
 apt-get install ddclient
 cp ddclient.in /usr/sbin/ddclient
+
+echo Please input the server url ( example: dynamicdns.key-systems.net ).
+read serverURL
+
+echo Please input your password.
+read password
+
+echo Please input your domainName ( example: host.example.com ).
+read domainName
+
+echo : "
+protocol=keysystems,  
+server=$(serverURL),  
+password='$(password)' 
+" > /etc/ddclient.conf  
+
+echo : "
+daemon=5m  
+syslog=yes  
+ssl=yes  
+pid=/var/run/ddclient/ddclient_eth0.pid  
+cache=/var/cache/ddclient/ddclient_eth0.cache  
+use=if, if=eth0  
+
+protocol=keysystems,
+server=$(serverURL),  
+password='$(password)'  
+$(domainName)
+" > /etc/ddclient/ddclient_eth0.conf
